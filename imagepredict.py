ed5e5c590c517cbe90cefa64d9f9dd99a2fe571a
Lester Tubo <106666919+lester18t@users.noreply.github.com>
2023-10-23 08:42:21 +0800

    test

--- imagepredict.py
+++ imagepredict.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
-model = YOLO('runs/detect/train3/weights/best.pt')
+model = YOLO('yolov8n.pt')
 
 # Run inference on 'bus.jpg' with arguments
 model.predict('face.jpg', save=True, imgsz=320, conf=0.2)
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
+dataset = 'https://universe.roboflow.com/ds/b3Pny8E8Ox?key=X3h3u1KTq1' #use dataset from generated roboflow using YOLOv8 format
+
 # Load a model
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
 
 # Train the model
-results = model.train(data='Face_Detector/data.yaml', resume=True, epochs=100, imgsz=640)
+results = model.train(data=dataset, resume=True, epochs=100, imgsz=640)
